e9add5a964ccdc430adc2690bb69d7a621834b94
Manuel Koell <manu@koell.li>
2020-01-25 12:02:59 +0100

    style grid tic tac toe like

--- src/components/Square.js
+++ src/components/Square.js
@@ -16,6 +16,31 @@ const Square = styled.button`
   &:hover {
     background: #eee;
   }
+
+  &:nth-child(1) {
+    border-width: 0 1px 1px 0;
+  }
+  &:nth-child(2) {
+    border-width: 0 1px 1px 1px;
+  }
+  &:nth-child(3) {
+    border-width: 0 0 1px 1px;
+  }
+  &:nth-child(4) {
+    border-width: 1px 1px 1px 0;
+  }
+  &:nth-child(6) {
+    border-width: 1px 0 1px 1px;
+  }
+  &:nth-child(7) {
+    border-width: 1px 1px 0 0;
+  }
+  &:nth-child(8) {
+    border-width: 1px 1px 0 1px;
+  }
+  &:nth-child(9) {
+    border-width: 1px 0 0 1px;
+  }
 `;
 
 export default ({ value, onClick }) => (
